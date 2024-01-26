569d3517a7e8bbfad5e98ef4a09f0bb16d40d06f
1Dohyeon <dh1072005@gmail.com>
2024-01-26 19:18:28 +0900

    예외처리를 위한 filter 파일 생성, 글로벌 설정

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/exception/httpException.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const PORT = process.env.PORT;
+
+  // 예외처리 글로벌 설정
+  app.useGlobalFilters(new HttpExceptionFilter());
 
   await app.listen(PORT);
 }
