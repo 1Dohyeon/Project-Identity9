7d359473bd79878689f63e99045995e95d4ffe62
1Dohyeon <dh1072005@gmail.com>
2024-01-29 12:37:51 +0900

    모듈 연결, 순환 모듈 문제 해결

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,18 +4,17 @@ import { MongooseModule } from '@nestjs/mongoose';
 import mongoose from 'mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { LoggerMiddleware } from './common/logger/logger.middleware';
-import { UserModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { LoggerMiddleware } from './common/logger/logger.middleware';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    UserModule,
+    UsersModule,
     MongooseModule.forRoot(process.env.MONGODB_URI),
-    UserModule,
     AuthModule,
   ],
   controllers: [AppController],
