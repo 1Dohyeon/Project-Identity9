f4f3867a386d9b7e2980799234fd33035421047d
1Dohyeon <dh1072005@gmail.com>
2024-02-07 23:39:27 +0900

    url 연결 테스트

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,8 +1,20 @@
 import { ThemeProvider } from "@mui/material";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import HomePage from "./pages/home.page";
+import UserPage from "./pages/user.page";
 import { theme } from "./shared/utils/theme";
 
 function App() {
-  return <ThemeProvider theme={theme}>hi</ThemeProvider>;
+  return (
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/user" element={<UserPage />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
+  );
 }
 
 export default App;
--- ui/src/pages/home.page.tsx
+++ ui/src/pages/home.page.tsx
@@ -1,5 +1,5 @@
-const homePage = () => {
+const HomePage = () => {
   return <div>home.page</div>;
 };
 
-export default homePage;
+export default HomePage;
