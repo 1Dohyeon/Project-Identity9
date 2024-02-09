9f26a9dfd9d91cd8b1f8b5fd138b0fc7e08c5d68
1Dohyeon <dh1072005@gmail.com>
2024-02-09 14:03:24 +0900

    schema 설명, schema에 data 추가

--- api/src/users/users.schema.ts
+++ api/src/users/users.schema.ts
@@ -1,5 +1,11 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { IsEmail, IsNotEmpty, IsString, Length } from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  Length,
+} from 'class-validator';
 import { Document, SchemaOptions } from 'mongoose';
 
 // createdAt, updatedAt field
@@ -9,6 +15,7 @@ const options: SchemaOptions = {
 
 @Schema(options)
 export class User extends Document {
+  // 사용자 email(로그인시 id 역할, 중복x)
   @Prop({
     required: true,
     unique: true,
@@ -17,6 +24,7 @@ export class User extends Document {
   @IsNotEmpty()
   email: string;
 
+  // 사용자 패스워드
   @Prop({
     required: true,
   })
@@ -25,6 +33,7 @@ export class User extends Document {
   @Length(6, 20)
   password: string;
 
+  // 사용자 이름
   @Prop({
     required: true,
   })
@@ -33,6 +42,7 @@ export class User extends Document {
   @Length(2, 20)
   name: string;
 
+  // 사용자 닉네임(중복x)
   @Prop({
     required: true,
     unique: true,
@@ -42,15 +52,62 @@ export class User extends Document {
   @Length(4, 16)
   nickname: string;
 
+  // 사용자 프로필 사진
   @Prop()
   @IsString()
   profileImgUrl: string;
+
+  // 사용자 페이지의 게시물 갯수(최대 9개)
+  @Prop({
+    required: true,
+    default: 0,
+  })
+  @IsNumber()
+  pageArticlesCount: number;
+
+  // 사용자의 public 게시물 갯수(페이지에 올라갈 게시물, 따라서 최대 9개
+  // but 유료버전으로 페이지에 안띄우고 공개만 해둘 수도 있음)
+  @Prop({
+    required: true,
+    default: 0,
+  })
+  @IsNumber()
+  publicArticlesCount: number;
+
+  // 사용자의 private 게시물 갯수
+  @Prop({
+    required: true,
+    default: 0,
+  })
+  @IsNumber()
+  privateArticlesCount: number;
+
+  // 사용자 전체 게시물 갯수
+  @Prop({
+    required: true,
+    default: 0,
+  })
+  @IsNumber()
+  allArticlesCount: number;
 
   readonly readOnlyData: {
     id: string;
     email: string;
     name: string;
     nickname: string;
+  };
+
+  readonly readOnlyDataWithArticles: {
+    id: string;
+    email: string;
+    name: string;
+    nickname: string;
+    articles: {
+      pageArticlesCount: number;
+      allArticlesCount: number;
+      publicArticlesCount: number;
+      privateArticlesCount: number;
+    };
   };
 }
 
